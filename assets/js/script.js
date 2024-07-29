56e728c5496836d98158c8e6bee10d14a4d51678
AlphaOmega <ardionaschool1223@gmail.com>
2024-07-29 20:48:45 +0700

    menambahkan breakpoints dan sedikit javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 let menu = document.getElementById("menu");
 let sideBar = document.getElementById("sidebar");
 menu.addEventListener("click", function () {
-  sideBar.classList.toggle("open");
-  menu.classList.toggle("open");
+    sideBar.classList.toggle("open");
+    menu.classList.toggle("open");
+});
+
+document.addEventListener("click", function (e) {
+    if (!menu.contains(e.target)) {
+        sideBar.classList.remove("open");
+    }
 });
