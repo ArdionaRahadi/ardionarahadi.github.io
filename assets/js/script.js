56afb9451c4ebf69509a7ec4cf372af8a93c324b
AlphaOmega <ardionaschool1223@gmail.com>
2024-07-31 06:34:16 +0700

    menambahkan notification and setting ketika profil di klik

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,21 @@
 let menu = document.getElementById("menu");
 let sideBar = document.getElementById("sidebar");
+let imgThumb = document.getElementById("img_thumb");
+let profileSetting = document.getElementById("profile_setting");
 menu.addEventListener("click", function () {
     sideBar.classList.toggle("open");
     menu.classList.toggle("open");
 });
 
-document.addEventListener("click", function (e) {
+imgThumb.addEventListener("click", function () {
+    profileSetting.classList.toggle("show");
+});
+
+window.addEventListener("click", function (e) {
     if (!menu.contains(e.target)) {
         sideBar.classList.remove("open");
+    }
+    if (!imgThumb.contains(e.target)) {
+        profileSetting.classList.remove("show");
     }
 });
