cff7b7821ea77ebe3100691cb73afd28f50408fc
ardiona <ardionarahadi@gmail.com>
2024-10-27 12:03:59 +0700

    menambahkan gsap

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
 new Typewriter("#typewriter", {
-    strings: ["Jungler", "Mid Laner", "Exp Laner", "Gold Laner"],
-    autoStart: true,
-    loop: true
+  strings: [
+    "Web Developer",
+    "Programmer",
+    "Content Creator",
+    "Gammer",
+    "Jungler",
+    "Gold Laner",
+    "Exp Laner",
+    "Mid Laner",
+    "Roamer",
+  ],
+  autoStart: true,
+  loop: true,
+});
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.from(".illustration", {
+  duration: 1.5,
+  x: 200,
+  opacity: 0,
+  ease: "back.out(1.7)",
+});
+
+gsap.from(".welcome-content", {
+  duration: 1.5,
+  x: -200,
+  opacity: 0,
+  ease: "power2.out",
+});
+
+gsap.from("nav", {
+  duration: 1.2,
+  y: -100,
+  opacity: 0,
+  ease: "power2.out",
 });
