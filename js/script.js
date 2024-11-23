e7641be71e9edd70d7712808bbd173923d605edd
AlphaOmega <ardionabd08@gmail.com>
2024-11-23 21:48:29 +0700

    Menambahkan Animasi

--- js/script.js
+++ js/script.js
@@ -4,24 +4,24 @@ new Typewriter("#typewriter", {
     loop: true
 });
 
-// gsap.registerPlugin(ScrollTrigger);
-// gsap.from(".illustration", {
-//   duration: 1.5,
-//   x: 200,
-//   opacity: 0,
-//   ease: "back.out(1.7)",
-// });
+gsap.registerPlugin(ScrollTrigger);
+gsap.from(".illustration", {
+  duration: 1.5,
+  x: 200,
+  opacity: 0,
+  ease: "back.out(1.7)",
+});
 
-// gsap.from(".welcome-content", {
-//   duration: 1.5,
-//   x: -200,
-//   opacity: 0,
-//   ease: "power2.out",
-// });
+gsap.from(".welcome-content", {
+  duration: 1.5,
+  x: -200,
+  opacity: 0,
+  ease: "power2.out",
+});
 
-// gsap.from("nav", {
-//   duration: 1.2,
-//   y: -100,
-//   opacity: 0,
-//   ease: "power2.out",
-// });
+gsap.from("nav", {
+  duration: 1.2,
+  y: -100,
+  opacity: 0,
+  ease: "power2.out",
+});
